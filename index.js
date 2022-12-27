c7a199604c5259d6b432ff260b2219a079292d51
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2022-12-27 17:11:17 -0600

    commit tree 5a2d9d332384719820d7a577b835cabb6e094da6

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const right = left + 40
+
+    if (right < 400) {
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
